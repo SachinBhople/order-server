f580b50815967cf545f488abd78577804f81688f
sachin <sachinb0841@gmail.com>
2024-12-26 12:59:58 +0530

    index changes

--- index.ts
+++ index.ts
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser())
 app.use(cors(
-    { origin: "*", credentials: true }
+    { origin: true, credentials: true }
 ))
 // Routes
 app.use("/api/order", orderrouter);
